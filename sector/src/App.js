c92df3090370eaacbdf493aa89eeb8c1c8f3bb18
Subho Karmakar <isubhokarma@gmail.com>
2019-06-14 19:53:38 +0530

    Intial commit/ Comps added
    React App created
    Components added

---
+++ sector/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  );
+};
+
+export default App;
