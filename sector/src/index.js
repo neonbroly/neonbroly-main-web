69252adcf740061c1f37f2a209dbfbb63c4c3b69
Subho Karmakar <isubhokarma@gmail.com>
2019-06-20 18:08:39 +0530

    WTF a minor spelling mistake :beers:

--- sector/src/index.js
+++ sector/src/index.js
@@ -8,7 +8,7 @@ import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { reduxFirestore, getFirestore } from "redux-firestore";
-import { reactreduxFirebase, getFirebase } from "react-redux-firebase";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 
 import fbConfig from "./config/fbConfig";
 
@@ -22,7 +22,7 @@ const store = createStore(
       })
     ),
     reduxFirestore(fbConfig),
-    reactreduxFirebase(fbConfig)
+    reactReduxFirebase(fbConfig)
   )
 );
 
