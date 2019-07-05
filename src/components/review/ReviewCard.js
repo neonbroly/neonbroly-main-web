5cd3b0e1c559625aacaa727705db06297ffee2f9
Subho Karmakar <isubhokarma@gmail.com>
2019-07-05 17:03:48 +0530

    Create ReviewCard.js

---
+++ src/components/review/ReviewCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class ReviewCard extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="border border-light shadow-sm p-3 mb-5 bg-light rounded-lg">
+            <p className="container h5 font-weight-normal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <p className="container font-weight-light">
+              Posted by Subho on 15th October, 12pm
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReviewCard;
