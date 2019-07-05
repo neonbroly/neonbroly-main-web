2b05e2ff8ab2681c3b52037c549f85b198374c36
Subho Karmakar <isubhokarma@gmail.com>
2019-07-05 17:03:20 +0530

    Updated View list summary card

--- src/components/asset/AssetSummaryCard.js
+++ src/components/asset/AssetSummaryCard.js
@@ -5,17 +5,15 @@ class AssetSummaryCard extends Component {
     return (
       <div className="container">
         <div className="row">
-          <blockquote className=" border border-light shadow-sm p-3 mb-5 bg-light rounded-lg">
-            <blockquote className="blockquote">
-              <p className="mb-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                posuere erat a ante.
-              </p>
-              <footer className="blockquote-footer">
-                <p>Posted by Subho on 15th October, 12pm</p>
-              </footer>
-            </blockquote>
-          </blockquote>
+          <div className="border border-light shadow-sm p-3 mb-5 bg-light rounded-lg">
+            <p className="container h5 font-weight-normal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat a ante.
+            </p>
+            <p className="container font-weight-light">
+              Posted by Subho on 15th October, 12pm
+            </p>
+          </div>
         </div>
       </div>
     );
