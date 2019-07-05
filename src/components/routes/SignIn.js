7e051d238a1d61fe0ef54a00c92ecf60041b4c42
Subho Karmakar <isubhokarma@gmail.com>
2019-07-05 21:23:13 +0530

    SignIn page created

---
+++ src/components/routes/SignIn.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="display-1">Sign In</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
